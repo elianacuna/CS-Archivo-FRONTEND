a0eff32553c06b9544baaa86bba33dde5018752c
Elian Acuña <elianacuna@MacBook-Pro-de-Elian.local>
2025-08-18 12:00:18 -0600

    Agrega estructura inicial del proyecto React

---
+++ src/routes/Main.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+
+import '../styles/global.css';
+
+
+const Main = () => {
+    {/* Variables */ }
+    const API_URL = import.meta.env.VITE_API_URL;
+    const navigate = useNavigate();
+};
+
+export default Main;
